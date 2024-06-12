01908b4170b01ba92c203defe2e2458e9efed799
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-11 18:05:23 -0700

    dft working

---
+++ dft.js
@@ -0,0 +1,25 @@
+function dft (x) {
+  let fourier = [];
+  const N = x.length;
+
+  for (let k = 0; k < N; k++) {
+
+    let re = 0;
+    let im = 0;
+
+    for (let n = 0; n < N; n++) {
+
+      let arg = (TWO_PI * k * n) / N;
+
+      re += x[n] * cos(arg);
+      im -= x[n] * sin(arg);
+    }
+
+    re /= N;
+    im /= N;
+
+    fourier[k] = {re, im};
+    
+  } 
+  return fourier ;
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,67 @@
+let x = [];
+let y = [];
+let fourierX;
+let fourierY;
+let time = 0;
+let path = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  // const skip = 8;
+  // for (let i = 0; i < drawing.length; i += skip) {
+  //   x.push(drawing[i].x);
+  //   y.push(drawing[i].y);
+  // }
+
+  y = [100, 100, 100, 0, 0, 0, 100, 100, 100, 0, 0, 0, 100, 100, 100, 0, 0, 0, 100, 100, 100, 0, 0, 0]; 
+
+  fourierX = dft(x);
+  fourierY = dft(y);
+
+  // fourierX.sort((a, b) => b.amp - a.amp);
+  // fourierY.sort((a, b) => b.amp - a.amp);
+}
+
+function epiCycles(x, y, rotation, fourier) {
+  for (let i = 0; i < fourier.length; i++) {
+    let prevx = x;
+    let prevy = y;
+    let freq = fourier[i].freq;
+    let radius = fourier[i].amp;
+    let phase = fourier[i].phase;
+    x += radius * cos(freq * time + phase + rotation);
+    y += radius * sin(freq * time + phase + rotation);
+
+    stroke(255, 100);
+    noFill();
+    ellipse(prevx, prevy, radius * 2);
+    stroke(255);
+    line(prevx, prevy, x, y);
+  }
+  return createVector(x, y);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  translate(150, 200);
+
+  // let x = 0;
+
+  
+  beginShape();
+  noFill();
+  for (let i = 0; i < path.length; i++) {
+    vertex(path[i].x, path[i].y);
+  }
+  endShape();
+
+  const dt = TWO_PI / fourierY.length;
+  time += dt;
+
+  if (time > TWO_PI) {
+    time = 0;
+    path = [];
+  }
+
 }
+
